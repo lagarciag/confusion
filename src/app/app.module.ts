69343f48dcbb672b88f01c190a7de7b3971be4b0
Luis Garcia <luis.a.garcia@hpe.com>
2020-04-19 10:27:48 -0600

    angular routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import "hammerjs";
 
-import { AppComponent } from "./app.component";
+// external components
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import "hammerjs";
-import { MenuComponent } from "./menu/menu.component";
 import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+// internal components
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
-import { DishService } from "./services/dish.service";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+
+// internal services components
+import { DishService } from "./services/dish.service";
+
+// internal modules
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
     declarations: [
@@ -28,6 +37,7 @@ import { AboutComponent } from './about/about.component';
         FooterComponent,
         HomeComponent,
         AboutComponent,
+        ContactComponent,
     ],
     imports: [
         BrowserModule,
@@ -39,6 +49,7 @@ import { AboutComponent } from './about/about.component';
         MatCardModule,
         MatButtonModule,
         FontAwesomeModule,
+        AppRoutingModule,
     ],
     providers: [DishService],
     bootstrap: [AppComponent],
